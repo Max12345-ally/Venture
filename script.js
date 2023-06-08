8951895dfd9ecdb7f84d6b66b4c4ba78f2f75a98
maxbond <ceo@rf.ru>
2023-06-08 23:22:23 +0300

    bugs fixing

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ const mqDark = window.matchMedia("(prefers-color-scheme: dark)")
 const darkModeToggle = document.querySelector("a.dark-mode-toggle")
 const darkModeToggleText = darkModeToggle.querySelector("span")
 
+const menuToggle = document.querySelector("a.menu-toggle")
+const menuToggleText = menuToggle.querySelector("span")
 
+const bodyTag = document.querySelector("body")
+
+menuToggle.addEventListener("click", function () {
+    bodyTag.classlist.toggle("nav-open")
+})
 
 const updateDarkMode = function () {
     if (mqDark.matches) {
