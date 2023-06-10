51880ffd868d51a4be9c076bd6526d0cc9c850dc
maxbond <ceo@rf.ru>
2023-06-10 23:38:00 +0300

    bugs fixed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const mqDark = window.matchMedia("(prefers-color-scheme: dark)")
+// const mqDark = window.matchMedia("(prefers-color-scheme: dark)")
 
-const darkModeToggle = document.querySelector("a.dark-mode-toggle")
-const darkModeToggleText = darkModeToggle.querySelector("span")
+// const darkModeToggle = document.querySelector("a.dark-mode-toggle")
+// const darkModeToggleText = darkModeToggle.querySelector("span")
 
 const menuToggle = document.querySelector("a.menu-toggle")
 const menuToggleText = menuToggle.querySelector("span")
@@ -12,14 +12,14 @@ menuToggle.addEventListener("click", function () {
     bodyTag.classlist.toggle("nav-open")
 })
 
-const updateDarkMode = function () {
-    if (mqDark.matches) {
-        darkModeToggleText.innerHTML = "Light mode"
-    } 
+// const updateDarkMode = function () {
+//     if (mqDark.matches) {
+//         darkModeToggleText.innerHTML = "Light mode"
+//     } 
     
-}
+// }
 
-updateDarkMode()
-mqDark.addListener(function () {
-    updateDarkMode()
-})
+// updateDarkMode()
+// mqDark.addListener(function () {
+//     updateDarkMode()
+// })
