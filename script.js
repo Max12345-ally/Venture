163d30358c0732dd3799572d0d8b7ee07bc201a5
maxbond <ceo@rf.ru>
2023-07-11 23:20:41 +0300

    GSAP animation added

--- script.js
+++ script.js
@@ -10,6 +10,23 @@ const bodyTag = document.querySelector("body")
 
 menuToggle.addEventListener("click", function () {
     bodyTag.classList.toggle("nav-open")
+
+    if (bodyTag.classList.contains("nav-open")) {
+        menuToggleText.innerHTML = "Close"
+
+        gsap.to(".burger-top", {rotation: 45, transformOrigin: "50% 50%", y: 8 })
+        gsap.to(".burger-bottom", {rotation: -45, transformOrigin: "50% 50%", y: -8 })
+        gsap.to(".burger-mid", { width : 0})
+
+    } else {
+        menuToggleText.innerHTML = "Menu"
+
+        gsap.to(".burger-top", {rotation: 0, y: 0 })
+        gsap.to(".burger-bottom", {rotation: 0, y: 0 })
+        gsap.to(".burger-mid", {width: 28 })
+    }
+
+    
 })
 
 // const updateDarkMode = function () {
